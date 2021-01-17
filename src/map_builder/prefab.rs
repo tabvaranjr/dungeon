1116b620e2833b748ddf061403f83ae522d27110
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2021-01-16 21:49:53 -0500

    Fixed prefab placement logic that led to crashes. * The condition was inverted and the rectangle is always inside the bounds of the map.

--- src/map_builder/prefab.rs
+++ src/map_builder/prefab.rs
@@ -32,18 +32,18 @@ pub fn apply_prefab(mb: &mut MapBuilder, rng: &mut RandomNumberGenerator) {
     let mut attempts = 0;
     while placement.is_none() && attempts < 10 {
         let dimensions = Rect::with_size(
-            rng.range(0, SCREEN_WIDTH - FORTRESS.1),
-            rng.range(0, SCREEN_HEIGHT - FORTRESS.2),
+            rng.range(FORTRESS.1, SCREEN_WIDTH - FORTRESS.1),
+            rng.range(FORTRESS.2, SCREEN_HEIGHT - FORTRESS.2),
             FORTRESS.1,
             FORTRESS.2,
         );
 
-        let mut can_place = false;
+        let mut can_place = true;
         dimensions.for_each(|pt| {
             let idx = mb.map.point2d_to_index(pt);
             let distance = dijkstra_map.map[idx];
-            if distance < 2000.0 && distance > 20.0 && mb.amulet_start != pt {
-                can_place = true;
+            if distance > 2000.0 || distance < 20.0 || mb.amulet_start == pt {
+                can_place = false;
             }
         });
 
