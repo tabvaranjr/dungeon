8c6fb3aa94f8a67a43b952b37b9dd71e48854095
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2021-02-04 15:06:14 -0500

    Finished first section of Chapter 15.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod camera;
 mod components;
 mod map;
 mod map_builder;
-mod spawners;
+mod spawner;
 mod systems;
 mod turn_state;
 
@@ -22,7 +22,7 @@ pub mod prelude {
     pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
-    pub use crate::spawners::*;
+    pub use crate::spawner::*;
     pub use crate::systems::*;
     pub use crate::turn_state::*;
 
@@ -50,13 +50,11 @@ impl State {
         let mut map_builder = MapBuilder::build(&mut rng);
 
         spawn_player(&mut ecs, map_builder.player_start);
-        //spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
+
+        spawn_level(&mut ecs, &mut rng, 0, &map_builder.monster_spawns);
+
         let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
         map_builder.map.tiles[exit_idx] = TileType::Exit;
-
-        map_builder.monster_spawns.iter().for_each(|pos| {
-            spawn_entity(&mut ecs, &mut rng, *pos);
-        });
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
@@ -79,18 +77,10 @@ impl State {
         let mut map_builder = MapBuilder::build(&mut rng);
 
         spawn_player(&mut self.ecs, map_builder.player_start);
-        //spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+        spawn_level(&mut self.ecs, &mut rng, 0, &map_builder.monster_spawns);
+
         let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
         map_builder.map.tiles[exit_idx] = TileType::Exit;
-
-        map_builder
-            .rooms
-            .iter()
-            .skip(1)
-            .map(|r| r.center())
-            .for_each(|pos| {
-                spawn_entity(&mut self.ecs, &mut rng, pos);
-            });
 
         self.resources.insert(map_builder.map);
         self.resources.insert(Camera::new(map_builder.player_start));
@@ -202,10 +192,8 @@ impl State {
             map_builder.map.tiles[exit_idx] = TileType::Exit;
         }
 
-        map_builder
-            .monster_spawns
-            .iter()
-            .for_each(|pos| spawn_entity(&mut self.ecs, &mut rng, *pos));
+        spawn_level(&mut self.ecs, &mut rng, map_level as usize, &map_builder.monster_spawns);
+
         self.resources.insert(map_builder.map);
         self.resources.insert(Camera::new(map_builder.player_start));
         self.resources.insert(TurnState::AwaitingInput);
