3b00d9e630b528a0b2b13916314f8637bc677ddf
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2021-02-06 12:35:18 -0500

    Moved State from main to state module.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod components;
 mod map;
 mod map_builder;
 mod spawner;
+mod state;
 mod systems;
 mod turn_state;
 
@@ -23,6 +24,7 @@ pub mod prelude {
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::spawner::*;
+    pub use crate::state::*;
     pub use crate::systems::*;
     pub use crate::turn_state::*;
 
@@ -33,223 +35,6 @@ pub mod prelude {
 }
 
 use prelude::*;
-
-struct State {
-    ecs: World,
-    resources: Resources,
-    input_systems: Schedule,
-    player_systems: Schedule,
-    monster_systems: Schedule,
-}
-
-impl State {
-    fn new() -> Self {
-        let mut ecs = World::default();
-        let mut resources = Resources::default();
-        let mut rng = RandomNumberGenerator::new();
-        let mut map_builder = MapBuilder::build(&mut rng);
-
-        spawn_player(&mut ecs, map_builder.player_start);
-
-        spawn_level(&mut ecs, &mut rng, 0, &map_builder.monster_spawns);
-
-        let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
-        map_builder.map.tiles[exit_idx] = TileType::Exit;
-
-        resources.insert(map_builder.map);
-        resources.insert(Camera::new(map_builder.player_start));
-        resources.insert(TurnState::AwaitingInput);
-        resources.insert(map_builder.theme);
-
-        Self {
-            ecs,
-            resources,
-            input_systems: build_input_scheduler(),
-            player_systems: build_player_scheduler(),
-            monster_systems: build_monster_scheduler(),
-        }
-    }
-
-    fn reset_game_state(&mut self) {
-        self.ecs = World::default();
-        self.resources = Resources::default();
-        let mut rng = RandomNumberGenerator::new();
-        let mut map_builder = MapBuilder::build(&mut rng);
-
-        spawn_player(&mut self.ecs, map_builder.player_start);
-        spawn_level(&mut self.ecs, &mut rng, 0, &map_builder.monster_spawns);
-
-        let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
-        map_builder.map.tiles[exit_idx] = TileType::Exit;
-
-        self.resources.insert(map_builder.map);
-        self.resources.insert(Camera::new(map_builder.player_start));
-        self.resources.insert(TurnState::AwaitingInput);
-        self.resources.insert(map_builder.theme);
-    }
-
-    fn victory(&mut self, ctx: &mut BTerm) {
-        ctx.set_active_console(HUD_LAYER);
-
-        ctx.print_color_centered(2, RED, BLACK, "VICTOLY!");
-        ctx.print_color_centered(
-            4,
-            WHITE,
-            BLACK,
-            "You put on your wizard hat and the amulet of Yala.",
-        );
-        ctx.print_color_centered(
-            5,
-            WHITE,
-            BLACK,
-            "Power over 9000 course through your veins and you pwn every evil beast.",
-        );
-        ctx.print_color_centered(6, WHITE, BLACK, "Time for retirement!");
-        ctx.print_color_centered(9, GREEN, BLACK, "Press '1' to regret thy life again");
-
-        if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.reset_game_state();
-        }
-    }
-
-    fn game_over(&mut self, ctx: &mut BTerm) {
-        ctx.set_active_console(HUD_LAYER);
-
-        ctx.print_color_centered(2, RED, BLACK, "Thou art dead!");
-        ctx.print_color_centered(
-            4,
-            WHITE,
-            BLACK,
-            "Slain and teabagged by a monster, thou art shamed ungracefully.",
-        );
-        ctx.print_color_centered(
-            5,
-            WHITE,
-            BLACK,
-            "The Amulet of Yala remains unclaimed, and your hometown is getting seriously pwn3d.",
-        );
-        ctx.print_color_centered(
-            8,
-            YELLOW,
-            BLACK,
-            "Does thy want to get brutally slain again?",
-        );
-        ctx.print_color_centered(9, GREEN, BLACK, "Press '1' to regret thy life again.");
-
-        if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.reset_game_state();
-        }
-    }
-
-    fn advance_level(&mut self) {
-        let player_entity = <Entity>::query()
-            .filter(component::<Player>())
-            .iter(&self.ecs)
-            .nth(0)
-            .unwrap();
-
-        use std::collections::HashSet;
-        let mut entities_to_keep = HashSet::new();
-        entities_to_keep.insert(*player_entity);
-
-        <(Entity, &Carried)>::query()
-            .iter(&self.ecs)
-            .filter(|(_, carry)| carry.0 == *player_entity)
-            .map(|(e, _)| e)
-            .for_each(|e| {
-                entities_to_keep.insert(*e);
-            });
-
-        let mut cb = CommandBuffer::new(&self.ecs);
-        for e in Entity::query().iter(&self.ecs) {
-            if !entities_to_keep.contains(e) {
-                cb.remove(*e);
-            }
-        }
-        cb.flush(&mut self.ecs);
-
-        <&mut FieldOfView>::query()
-            .iter_mut(&mut self.ecs)
-            .for_each(|fov| fov.is_dirty = true);
-
-        let mut rng = RandomNumberGenerator::new();
-        let mut map_builder = MapBuilder::build(&mut rng);
-
-        let mut map_level = 0;
-        <(&mut Player, &mut Point)>::query()
-            .iter_mut(&mut self.ecs)
-            .for_each(|(player, pos)| {
-                player.map_level += 1;
-                map_level = player.map_level;
-                pos.x = map_builder.player_start.x;
-                pos.y = map_builder.player_start.y;
-            });
-
-        if map_level == 2 {
-            spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
-        } else {
-            let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
-            map_builder.map.tiles[exit_idx] = TileType::Exit;
-        }
-
-        spawn_level(
-            &mut self.ecs,
-            &mut rng,
-            map_level as usize,
-            &map_builder.monster_spawns,
-        );
-
-        self.resources.insert(map_builder.map);
-        self.resources.insert(Camera::new(map_builder.player_start));
-        self.resources.insert(TurnState::AwaitingInput);
-        self.resources.insert(map_builder.theme);
-    }
-}
-
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.set_active_console(MAP_LAYER);
-        ctx.cls();
-        ctx.set_active_console(DISPLAY_LAYER);
-        ctx.cls();
-        ctx.set_active_console(HUD_LAYER);
-        ctx.cls();
-
-        // Inputs.
-        ctx.set_active_console(MAP_LAYER);
-        self.resources.insert(ctx.key);
-        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
-
-        // Update.
-        let current_state = self.resources.get::<TurnState>().unwrap().clone();
-        match current_state {
-            TurnState::AwaitingInput => {
-                self.input_systems
-                    .execute(&mut self.ecs, &mut self.resources);
-            }
-            TurnState::PlayerTurn => {
-                self.player_systems
-                    .execute(&mut self.ecs, &mut self.resources);
-            }
-            TurnState::MonsterTurn => {
-                self.monster_systems
-                    .execute(&mut self.ecs, &mut self.resources);
-            }
-            TurnState::GameOver => {
-                self.game_over(ctx);
-            }
-            TurnState::Victory => {
-                self.victory(ctx);
-            }
-            TurnState::NextLevel => {
-                self.advance_level();
-            }
-        };
-
-        // Render.
-        render_draw_buffer(ctx).expect("Render error");
-    }
-}
 
 fn main() -> BError {
     let context = BTermBuilder::new()
@@ -265,5 +50,5 @@ fn main() -> BError {
         .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State::new())
+    main_loop(context, state::State::new())
 }
