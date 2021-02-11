c2767d6f91f1aae9ff2f8f0410a2f5af10446c90
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2021-02-10 21:22:11 -0500

    Added debug output in combat.

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -6,6 +6,7 @@ use crate::prelude::*;
 #[write_component(Health)]
 #[read_component(Damage)]
 #[read_component(Carried)]
+#[read_component(Name)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
 
@@ -15,11 +16,35 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .collect();
 
     victims.iter().for_each(|(message, attacker, victim)| {
-        let is_player = ecs
+        let is_player_victim = ecs
             .entry_ref(*victim)
             .unwrap()
             .get_component::<Player>()
             .is_ok();
+
+        let attacker_name = if let Ok(attacker) = ecs.entry_ref(*attacker) {
+            if attacker.get_component::<Player>().is_ok() {
+                String::from("Player")
+            } else if let Ok(name) = attacker.get_component::<Name>() {
+                name.0.clone()
+            } else {
+                String::from("Unknown")
+            }
+        } else {
+            String::from("Unknown")
+        };
+
+        let victim_name = if let Ok(victim) = ecs.entry_ref(*victim) {
+            if victim.get_component::<Player>().is_ok() {
+                String::from("Player")
+            } else if let Ok(name) = victim.get_component::<Name>() {
+                name.0.clone()
+            } else {
+                String::from("Unknown")
+            }
+        } else {
+            String::from("Unknown")
+        };
 
         let base_damage = if let Ok(v) = ecs.entry_ref(*attacker) {
             if let Ok(dmg) = v.get_component::<Damage>() {
@@ -39,13 +64,18 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
 
         let final_damage = base_damage + weapon_damage;
 
+        println!(
+            "{} attacks {} for {} damage.",
+            attacker_name, victim_name, final_damage
+        );
+
         if let Ok(mut health) = ecs
             .entry_mut(*victim)
             .unwrap()
             .get_component_mut::<Health>()
         {
             health.current -= final_damage;
-            if health.current < 1 && !is_player {
+            if health.current < 1 && !is_player_victim {
                 commands.remove(*victim);
             }
         }
