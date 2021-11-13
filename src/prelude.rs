22d84585d7824425efbabbdf639f1eec3a52f9a1
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2021-11-13 09:10:15 -0500

    Moved prelude to module.

---
+++ src/prelude.rs
@@ -0,0 +1,25 @@
+pub const SCREEN_WIDTH: i32 = 80;
+pub const SCREEN_HEIGHT: i32 = 50;
+pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+pub const HUD_WIDTH: i32 = SCREEN_WIDTH;
+pub const HUD_HEIGHT: i32 = SCREEN_HEIGHT;
+pub const HUD_DISPLAY_RATIO: i32 = HUD_WIDTH / DISPLAY_WIDTH;
+
+pub const MAP_LAYER: usize = 0;
+pub const DISPLAY_LAYER: usize = 1;
+pub const HUD_LAYER: usize = 2;
+
+pub use crate::camera::*;
+pub use crate::components::*;
+pub use crate::map::*;
+pub use crate::map_builder::*;
+pub use crate::spawner::*;
+pub use crate::state::*;
+pub use crate::systems::*;
+pub use crate::turn_state::*;
+
+pub use bracket_lib::prelude::*;
+pub use legion::systems::CommandBuffer;
+pub use legion::world::SubWorld;
+pub use legion::*;
