813b2a5499d2916e04edf4502eace31f95d4e66e
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2022-05-22 22:25:07 -0400

    Replaced sprites and backgrounds with equivalents from the DawnLike tileset on OpenGameArt.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A basic dungeon crawler in Rust based on the beta version of [Hands-On Rust eBook](https://pragprog.com/titles/hwrust/hands-on-rust/).
 
 ![Rust](https://github.com/tabvaranjr/dungeon/workflows/Rust/badge.svg)
+
+## Attributions
+* The font comes from the code provided with the book.
+* Artwork was taken from the [DawnLike rogue-like tileset](https://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181) (CC-BY 4.0)
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> BError {
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
-        .with_font("dungeonfont.png", 32, 32)
+        .with_font("dungeonfont.png", 16, 16)
         .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
